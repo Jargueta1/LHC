f20b4ae1716ed006202e18cbfae941c83240041d
Jorge Argueta <67116860+Jargueta1@users.noreply.github.com>
2021-10-04 05:49:55 -0400

    added ability to save results to file

--- ana.C
+++ ana.C
@@ -3,6 +3,7 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include <TLorentzVector.h>
 
 void ana::Loop()
 {
@@ -30,15 +31,17 @@ void ana::Loop()
 //    fChain->GetEntry(jentry);       //read all branches
 //by  b_branchname->GetEntry(ientry); //read only this branch
    if (fChain == 0) return;
-   
+   TFile* output = TFile::Open("Dielectron_MC.root", "RECREATE"); // "RECREATE" would produce 
+
    // Declare histograms
     TH1F* el1_eta = new TH1F("el1_eta", "Pseudorapidity of the most energetic electron candidate"
-    , 100, -4, 4);
+    , 100, -5, 5);
     TH1F* el2_eta = new TH1F("el2_eta", "Pseudorapidity of the second most energetic electron candidate"
     , 100, -4, 4);
     
     TH1F* el1_pt = new TH1F("el1_pt", "pT of the most energetic electron candidate", 100, 0, 200);
     TH1F* el2_pt = new TH1F("el2_pt", "pT of the second most energetic electron candidate", 100, 0, 200);
+	TH1F* Z_ee = new TH1F("Z_ee", "Di-electron candidate invariant mass", 200, 0, 200);
 
    Long64_t nentries = fChain->GetEntriesFast();
 
@@ -52,5 +55,17 @@ void ana::Loop()
       el1_pt->Fill(f_lept1_pt);
       el2_pt->Fill(f_lept2_pt);
 
+      TLorentzVector el1, el2;
+      el1.SetPtEtaPhiM(f_lept1_pt, f_lept1_eta, f_lept1_phi, 0.0);
+      el2.SetPtEtaPhiM(f_lept2_pt, f_lept2_eta, f_lept2_phi, 0.0);
+      TLorentzVector zCandidate = el1 + el2;
+      Z_ee->Fill(zCandidate.M());
+
    }
+   el1_eta->Write();
+   el2_eta->Write();
+   el1_pt->Write();
+   el2_pt->Write();
+   Z_ee->Write();
+   output->Close();
 }
