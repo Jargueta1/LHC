edb508a401df695631e0e7a7b203195871109adc
Jorge Argueta <67116860+Jargueta1@users.noreply.github.com>
2021-10-12 15:05:47 -0400

    attempt 3

--- py/ana.py
+++ py/ana.py
@@ -3,7 +3,6 @@ from ROOT import TFile, TH1F
 
 h = TH1F("myHist", "myTitle", 64, -4, 4)
 
-
 output = TFile("Dielectron_MC.root", "Recreate"); 
 
 el1_eta = TH1F("el1_eta", "Pseudorapidity of the most energetic electron candidate", 100, -5, 5);
@@ -12,6 +11,6 @@ el1_pt =  TH1F("el1_pt", "pT of the most energetic electron candidate", 100, 0, 
 el2_pt = TH1F("el2_pt", "pT of the second most energetic electron candidate", 100, 0, 200);
 Z_ee =  TH1F("Z_ee", "Di-electron candidate invariant mass", 200, 0, 200);
 
-el1_eta.FillRandom("f_lept1_eta",1000)
+el1_eta.FillRandom("x+1",1000)
 el1_eta.SetFillColor( 45 )
 el1_eta.Draw()
