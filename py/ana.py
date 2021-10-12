d57a4f4b8cf6dd1d5b035989a21797dbb99a340d
Jorge Argueta <67116860+Jargueta1@users.noreply.github.com>
2021-10-12 14:47:02 -0400

    attempt 1

--- py/ana.py
+++ py/ana.py
@@ -1,5 +1,16 @@
 import ROOT 
+from ROOT import TFile, TH1F
 
-h = ROOT.TH1F("myHist", "myTitle", 64, -4, 4)
+h = TH1F("myHist", "myTitle", 64, -4, 4)
 h.FillRandom("gaus")
+
+output = TFile("Dielectron_MC.root"); 
+
+el1_eta = TH1F("el1_eta", "Pseudorapidity of the most energetic electron candidate", 100, -5, 5);
+el2_eta = TH1F("el2_eta", "Pseudorapidity of the second most energetic electron candidate", 100, -4, 4);
+el1_pt =  TH1F("el1_pt", "pT of the most energetic electron candidate", 100, 0, 200);
+el2_pt = TH1F("el2_pt", "pT of the second most energetic electron candidate", 100, 0, 200);
+Z_ee =  TH1F("Z_ee", "Di-electron candidate invariant mass", 200, 0, 200);
+
+h.draw()
 
